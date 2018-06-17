766fcfc5b295934c9efd580242531ba277867214
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2018-06-17 17:51:16 +0300

    Exam

---
+++ Murder.js
@@ -0,0 +1,87 @@
+'use strict'
+// const  promise = new Promise((resolve, reject) => {
+//   ////async code
+//
+// });
+//
+// promise
+//   .then(
+//     result =>
+//     error => console.log(error.message)
+//   );
+// const async = require('async');
+//
+
+
+
+let mas = ["Bill", "Lucy", "Poll", "Ken", "Jen"];
+/////////////////Последовательно выполняется////////////////
+const Kill = mas => Promise.resolve(mas.shift());
+async function threaten2(mas) {
+  for (let i = 0; i < 1; i++) {
+    try {
+      let dead = await Kill(mas);
+      if (!dead) throw new Error("All died");
+      console.log("Dead " + dead);
+    } catch (err) {
+      throw err.message;
+    }
+  }
+  return mas;
+};
+threaten2(mas).then((resolve) => { console.log("They are next -> " + resolve.join(' -> '));});
+
+//////////// через 2 дня не выплатят выкуп////////////
+const noPay = mas => new Promise((resolve, reject) => {
+  if (!mas.length) reject('All died');
+  setTimeout(resolve, 100, mas.shift());
+});
+
+
+//промисы выполняются последовательно
+async function threaten(mas, murderMap) {
+  const killLucy = await noPay(mas);
+  const killPoll = await noPay(mas);
+  const killKen = await noPay(mas);
+  return mas;
+}
+threaten(mas);
+/////////////////// Without await//////////////////////
+// let killBill;
+// async function threatened (mas){
+//   return new Promise((resolve, reject) => {
+//     if(!mas.length) reject('All died');
+//     setTimeout(() => {
+//       resolve(mas.shift());
+//     }, 1000);
+//     }).then((result) => { killBill = resolve;});
+// }
+//2 промиса разрешаются паралельно
+//одновременно получить результат от нескольких вызовов
+
+const first = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "Dan");
+});
+
+const second = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "Rom");
+});
+
+const third = new Promise((resolve, reject) => {
+  setTimeout(resolve, 1000, "Tan");
+});
+// threaten(mas);
+
+Promise.all([first, second, third]).then(console.log).catch(console.log);
+
+const Kidnapping = human => Promise.resolve(human);
+
+const threaten1 = () => Kidnapping("Julia");
+
+threaten1()
+  .then(resolve => {
+    console.log(resolve + " died " );
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
